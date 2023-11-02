4fca13b8cdc7342af297cd226323102470f8cd69
19307130359 <19307130359@fudan.edu.cn>
2023-11-02 22:08:42 +0800

    Add 4 functions in data_analysis.py. Need to close one figure before open the other one.

--- Housing_price_analysis_system/data_analysis.py
+++ Housing_price_analysis_system/data_analysis.py
@@ -4,13 +4,133 @@
 # data cleaning and analysis
 # 对房价数据集（data.xlsx）进行清洗和数据分析，并进行可视化
 """
-
+import matplotlib.pyplot as plt
 import pandas as pd
+import re
 
+plt.rcParams['font.sans-serif'] = ['SimHei']  # 用来设置字体样式以正常显示中文标签（黑体）
+plt.rcParams['axes.unicode_minus'] = False  # 正确输出负数
 
 data = pd.read_csv('data.csv')
 pd.options.mode.chained_assignment = None  # default='warn'
-# print(data.head)
+# print(data.shape)   # (2583, 10)
+# print(data)
+
 
 # 发现有一整行记录为空值  ---> delete the row
-cleaned = data.dropna(how='all')  # 清除全为空的行
+data.drop(data.columns[0], axis=1, inplace=True)
+data.dropna(how='all', inplace=True)  # 清除全为空的行
+
+data['总价'] = [re.sub('[^0-9.]', '', price) for price in data['总价']]
+data['建筑面积'] = [re.sub('[^0-9.]', '', area) for area in data['建筑面积']]
+data['单价'] = [re.sub('[^0-9.]', '', price_per) for price_per in data['单价']]
+# Index(['小区名字', '总价', '户型', '建筑面积', '单价', '朝向', '楼层', '装修', '区域'], dtype='object')
+data['总价'] = data['总价'].astype(float)
+data['单价'] = data['单价'].astype(float)
+
+# filepath = 'data_cleaned.xlsx'
+# cleaned.to_excel(filepath, index=False)
+
+
+# 各区二手房均价分析
+# done
+def average_price():
+    # 均价计算
+    data1 = data.groupby(['区域'])['单价'].mean()
+
+    # 可视化
+    # 创建条形图
+    ax = data1.plot.bar()
+
+    # 添加标题和轴标签
+    plt.title('各区二手房均价分析')
+    plt.xlabel('区域', fontsize=15)
+    plt.ylabel('各区均价', fontsize=15)
+    plt.xticks(fontsize=12, rotation=0)  # 旋转30度
+    plt.yticks(fontsize=12)
+    # 在每个柱子上显示数值
+    for a, b in enumerate(data1):
+        plt.text(a, b, int(b), ha='center', va='bottom', fontsize=12)
+
+    # 显示图表
+    plt.show()
+
+
+# 各区二手房数量所占比例
+# done
+def number_proportion():
+    data2 = data['区域'].value_counts()
+
+    # 创建饼状图
+    data2.plot(kind='pie', autopct='%1.1f%%', startangle=90)
+    plt.legend(data2.index, title="图例", loc="upper right", bbox_to_anchor=(1.3, 1.1))
+
+    plt.title("各区二手房数量所占比例")
+    plt.ylabel('')  # 这将删除标签“values”，使图看起来更干净
+    plt.show()
+
+    # print(data2)
+
+
+# 全市二手房装修程度分析
+# done
+def house_decoration():
+    data3 = data['装修'].value_counts()
+
+    # 可视化
+    # 创建条形图
+    ax = data3.plot.bar()
+
+    # 添加标题和轴标签
+    plt.title('全市二手房装修程度分析')
+    plt.xlabel('装修', fontsize=15)
+    plt.ylabel('套数', fontsize=15)
+    plt.xticks(fontsize=12, rotation=0)  # 旋转30度
+    plt.yticks(fontsize=12)
+    # 在每个柱子上显示数值
+    for a, b in enumerate(data3):
+        plt.text(a, b, int(b), ha='center', va='bottom', fontsize=12)
+
+    # 显示图表
+    plt.show()
+
+
+# 热门户型均价分析
+# done
+def type_average_price():
+    num_per_type = data['户型'].value_counts()
+
+    # print(data4.index)
+    # print(data4.columns)
+    # print(num_per_type)
+    popular_type = num_per_type[0:6].index.tolist()
+    # print(popular_type)
+    data4 = data.groupby(['户型'])['单价'].mean()
+    data4 = data4[popular_type]
+
+
+    # 可视化
+    # 创建条形图
+    ax = data4.plot.bar()
+
+    # 添加标题和轴标签
+    plt.title('热门户型均价分析')
+    plt.xlabel('户型', fontsize=15)
+    plt.ylabel('均价', fontsize=15)
+    plt.xticks(fontsize=12, rotation=0)  # 旋转30度
+    plt.yticks(fontsize=12)
+    # 在每个柱子上显示数值
+    for a, b in enumerate(data4):
+        plt.text(a, b, int(b), ha='center', va='bottom', fontsize=12)
+
+    # 显示图表
+    plt.show()
+
+
+# 二手房售价预测
+def price_prediction():
+    print("5")
+
+
+if __name__ == '__main__':
+    number_proportion()
--- Housing_price_analysis_system/main.py
+++ Housing_price_analysis_system/main.py
@@ -6,6 +6,8 @@ import sys
 from PyQt5.QtWidgets import QMainWindow, QApplication
 from system_menu import Ui_MainWindow
 from PyQt5.QtCore import Qt
+from data_analysis import average_price, number_proportion, house_decoration, type_average_price, price_prediction
+import threading
 
 
 class MyPyQT_Form(QMainWindow, Ui_MainWindow):
@@ -13,41 +15,47 @@ class MyPyQT_Form(QMainWindow, Ui_MainWindow):
         super(MyPyQT_Form, self).__init__()
         self.setupUi(self)
 
-    # 实现pushButton_click()函数，textEdit是我们放上去的文本框的id
-    # def pushButton_click_test(self):
-    #     self.textEdit.setText("你点击了按钮")
-
     # 各区二手房均价分析
     @staticmethod
-    def average_price(self):
-        print("hi")
+    def act1_average_price(self):
+        average_price()
+        # print("hi")
 
     # 各区二手房数量所占比例
     @staticmethod
-    def number_proportion(self):
-        print("2")
+    def act2_number_proportion(self):
+        number_proportion()
+        # print("2")
 
     # 全市二手房装修程度分析
     @staticmethod
-    def house_decoration(self):
-        print("3")
+    def act3_house_decoration(self):
+        house_decoration()
+        # print("3")
 
     # 热门户型均价分析
     @staticmethod
-    def type_average_price(self):
-        print("4")
+    def act4_type_average_price(self):
+        type_average_price()
+        # print("4")
 
     # 二手房售价预测
     @staticmethod
-    def price_prediction(self):
-        print("5")
+    def act5_price_prediction(self):
+        price_prediction()
+        # print("5")
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     app.setAttribute(Qt.AA_EnableHighDpiScaling, True)   # 启用或禁用高DPI缩放
     my_pyqt_form = MyPyQT_Form()
-    my_pyqt_form.action_1.triggered.connect(my_pyqt_form.average_price)
+
+    my_pyqt_form.action_1.triggered.connect(my_pyqt_form.act1_average_price)
+    my_pyqt_form.action_2.triggered.connect(my_pyqt_form.act2_number_proportion)
+    my_pyqt_form.action_3.triggered.connect(my_pyqt_form.act3_house_decoration)
+    my_pyqt_form.action_4.triggered.connect(my_pyqt_form.act4_type_average_price)
+    my_pyqt_form.action_5.triggered.connect(my_pyqt_form.act5_price_prediction)
 
     my_pyqt_form.show()
     sys.exit(app.exec_())
